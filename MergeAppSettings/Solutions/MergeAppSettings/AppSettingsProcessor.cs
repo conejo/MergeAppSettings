7208060742ba3ad2b1e81feac085ff85ceac1cfc
Rich Cox <conejo2005@gmail.com>
2014-12-03 22:16:31 -0700

    add solution

---
+++ MergeAppSettings/Solutions/MergeAppSettings/AppSettingsProcessor.cs
@@ -0,0 +1,51 @@
+﻿namespace MergeAppSettings
+{
+    using System;
+    using System.Configuration;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    public class AppSettingsProcessor
+    {
+        private readonly string source;
+
+        private readonly string target;
+
+        public AppSettingsProcessor(string source, string target)
+        {
+            this.source = source;
+            this.target = target;
+        }
+
+        public void Process()
+        {
+            var sourceDoc = XDocument.Load(this.source);
+            var targetDoc = XDocument.Load(this.target);
+
+            var sourceAppSettings = sourceDoc.Descendants("appSettings");
+            var targetAppSettings = targetDoc.Descendants("appSettings");
+
+            foreach (var element in sourceAppSettings.Descendants())
+            {
+                Console.WriteLine("key: {0}, value: {1}", element.Attribute("key").Value, element.Attribute("value").Value);
+
+                if (targetAppSettings.Descendants().Any(x => x.Attribute("key").Value == element.Attribute("key").Value))
+                {
+                    Console.WriteLine("update value for existing key: {0}", element.Attribute("key").Value);
+                    var updateElement = targetAppSettings.Descendants().First(x => x.Attribute("key").Value == element.Attribute("key").Value);
+                    updateElement.SetAttributeValue("value", element.Attribute("value").Value);
+                }
+                else
+                {
+                    Console.WriteLine("add key for: {0}", element.Attribute("key").Value);
+                    var newElement = new XElement("add");
+                    newElement.SetAttributeValue("key", element.Attribute("key").Value);
+                    newElement.SetAttributeValue("value", element.Attribute("value").Value);
+                    targetAppSettings.First().Add(newElement);
+                }
+            }
+
+            targetDoc.Save(this.target);
+        }
+    }
+}
---
+++ MergeAppSettings/Solutions/MergeAppSettings/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeAppSettings
+{
+    using System.IO;
+    using System.Security.Cryptography;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Please enter shared and target config file names.");
+                Console.ReadKey();
+                return;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("Can't find: {0}", args[0]);
+                Console.ReadKey();
+                return;
+            }
+
+            if (!File.Exists(args[1]))
+            {
+                Console.WriteLine("Can't find: {0}", args[1]);
+                Console.ReadKey();
+                return;
+            }
+
+            
+
+            var processor = new AppSettingsProcessor(args[0], args[1]);
+            processor.Process();
+
+            Console.ReadKey();
+        }
+    }
+}
